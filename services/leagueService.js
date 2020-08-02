2528bc4623013d241c6c500a5edac20fec3926b6
ptoninato <pmtoninato@gmail.com>
2020-08-02 00:07:38 -0500

    insert leagues

--- services/leagueService.js
+++ services/leagueService.js
@@ -5,24 +5,40 @@ import gameCodeTypeService from './gameCodeTypeService.js';
 import gameCodeService from './gameCodeService.js';
 
 const GetLeagueRecords = async () => {
-  const query = leagueModel.select(leagueModel.star()).from(leagueModel).toQuery();
-  const result = await pool.query(query);
-  return result;
+  try {
+    const results = await pool.query('SELECT distinct leaguename FROM leagues');
+    return [...new Set(results.rows.map((item) => item.leaguename))];
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
+const insertYahooLeagues = async (leagues) => {
+  try {
+    console.log(leagues);
+    const query = leagueModel.insert(leagues).returning(leagueModel.leagueid).toQuery();
+    const { rows } = await pool.query(query);
+    console.log(rows);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    // client.end();
+  }
 };
 
 async function filterLeagueRecords(leagueRecords) {
   const exisitingGameCodes = await gameCodeTypeService.getAllCodeTypes();
+  const existingLeagueNames = await GetLeagueRecords();
   const leagues = []; const leaguesOutput = [];
   for (let i = 0; i < leagueRecords.length; i++) {
     for (let x = 0; x < leagueRecords[i].leagues.length; x++) {
-      const currentLeague = leagueRecords[i].leagues[x];
-      if (leagues[currentLeague[0].name]) continue;
-      // const gameCodeType = exisitingGameCodes.rows.find(({ yahoogamecode }) => yahoogamecode === currentLeague.game_code);
-      const gameCodeType = exisitingGameCodes.rows.filter((value) => value.yahoogamecode === currentLeague[0].game_code);
-      console.log(currentLeague[0].game_code);
-      console.log(gameCodeType);
-      leagues[currentLeague[0].name] = true;
-      leaguesOutput.push({ leaguename: currentLeague[0].name, gamecodetypeid: gameCodeType[0].gamecodetypeid });
+      const currentLeague = leagueRecords[i].leagues[x][0];
+      if (leagues[currentLeague.name]
+        && (existingLeagueNames.length > 0 || !existingLeagueNames.includes(currentLeague.name))) continue;
+      const gameCodeType = exisitingGameCodes.rows.filter((value) => value.yahoogamecode === currentLeague.game_code);
+      leagues[currentLeague.name] = true;
+      leaguesOutput.push({ leaguename: currentLeague.name, gamecodetypeid: gameCodeType[0].gamecodetypeid });
     }
   }
   return leaguesOutput;
@@ -32,9 +48,12 @@ const InsertLeagues = async (req, res) => {
   const results = await gameCodeService.getYahooGameCodes();
   const gameCodes = await results.map(Number);
   const leagueRecords = await yahooApiService.getUserLeaguesFromYahoo(req, res, gameCodes);
-  // const existingLeagueRecords = GetLeagueRecords();
-
   const leagues = await filterLeagueRecords(leagueRecords.data.games);
+
+  if (leagues.length > 0) {
+    console.log(leagues);
+    await insertYahooLeagues(leagues);
+  }
 
   return leagues;
 };
