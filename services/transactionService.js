1de80ca0a5fd75cde386f0200b9721f46bc2e571
ptoninato <pmtoninato@gmail.com>
2020-08-13 23:15:34 -0500

    check for exisiting records

--- services/transactionService.js
+++ services/transactionService.js
@@ -7,6 +7,16 @@ import transactionTypeService from './transactionTypeService.js';
 import viewService from './viewService.js';
 import positionTypeService from './positionTypeService.js';
 
+const GetSeasonAndTransactionIds = async () => {
+  try {
+    const results = await pool.query('select distinct seasonid, yahootransactionid from transaction');
+    return results;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+};
+
 const GetLeagueCodes = async () => {
   try {
     const results = await pool.query('select * from yahooleaguecode');
@@ -22,7 +32,7 @@ async function InsertTransactions(transactions) {
     const query = transactionModel.insert(transactions).returning(transactionModel.transactionid).toQuery();
     const { rows } = await pool.query(query);
   } catch (e) {
-    console.error(e);
+    console.error('fail');
   } finally {
     // client.end();
   }
@@ -35,6 +45,7 @@ const ImportTransactions = async (req, res) => {
   const teams = await viewService.GetAllYahooTeamKeys();
   let positiontypes = await positionTypeService.GetPositionTypes();
   let transactionsToImport = [];
+  const existingTransactions = await GetSeasonAndTransactionIds();
   for (let l = 0; l < leagueCodes.rows.length; l++) {
     const transactions = await yahooApiService.getLeagueTransactions(req, res, leagueCodes.rows[l].leaguecode);
     const leagueCodeType = transactions.data.game_code;
@@ -47,64 +58,66 @@ const ImportTransactions = async (req, res) => {
       const transaction = transactions.data.transactions[t];
       let playersFromDb = await playerSerivce.GetPlayers();
       let transactionTypesFromDb = await transactionTypeService.GetTransactionTypes();
-      for (let p = 0; p < transaction.players.length; p++) {
-        const transactionPlayer = transaction.players[p];
-        let existingPositionType = await positiontypes.rows.filter((x) => x.yahoopositiontype === transactionPlayer.position_type && x.gamecodetypeid === codeTypeId);
-        if (existingPositionType.length === 0) {
-          const positiontypetoadd = {
-            gamecodetypeid: codeTypeId,
-            yahoopositiontype: transactionPlayer.position_type
-          };
-          await positionTypeService.InsertPositionType(positiontypetoadd);
-          positiontypes = await positionTypeService.GetPositionTypes();
-          existingPositionType = await positiontypes.rows.filter((x) => x.yahoopositiontype === transactionPlayer.position_type && x.gamecodetypeid === codeTypeId);
-        }
+      const existingTransaction = await existingTransactions.rows.filter((x) => x.yahootransactionid === Number(transaction.transaction_id) && x.seasonid === seasonId);
+      if (existingTransaction.length === 0) {
+        for (let p = 0; p < transaction.players.length; p++) {
+          const transactionPlayer = transaction.players[p];
+          let existingPositionType = await positiontypes.rows.filter((x) => x.yahoopositiontype === transactionPlayer.position_type && x.gamecodetypeid === codeTypeId);
+          if (existingPositionType.length === 0) {
+            const positiontypetoadd = {
+              gamecodetypeid: codeTypeId,
+              yahoopositiontype: transactionPlayer.position_type
+            };
+            await positionTypeService.InsertPositionType(positiontypetoadd);
+            positiontypes = await positionTypeService.GetPositionTypes();
+            existingPositionType = await positiontypes.rows.filter((x) => x.yahoopositiontype === transactionPlayer.position_type && x.gamecodetypeid === codeTypeId);
+          }
 
-        let existingPlayers = await playersFromDb.rows.filter((x) => x.yahooplayerid === Number(transactionPlayer.player_id) && x.gamecodetypeid === codeTypeId);
-        if (existingPlayers.length === 0) {
-          const playerToAdd = {
-            gamecodetypeid: codeTypeId,
-            yahooplayerid: transactionPlayer.player_id,
-            firstname: transactionPlayer.name.first,
-            lastname: transactionPlayer.name.last.length > 0 ? transactionPlayer.name.last : 'Defense',
-            positiontypeid: existingPositionType[0].positiontypeid
-          };
-          await playerSerivce.InsertPlayer(req, res, playerToAdd);
-          playersFromDb = await playerSerivce.GetPlayers();
-          existingPlayers = await playersFromDb.rows.filter((x) => x.yahooplayerid === Number(transactionPlayer.player_id) && x.gamecodetypeid === codeTypeId);
-        }
-        let existingTransactionTypes = await transactionTypesFromDb.rows.filter((x) => x.transactiontypename === transactionPlayer.transaction.type);
-        if (existingTransactionTypes.length === 0) {
-          const typeToAdd = {
-            transactiontypename: transactionPlayer.transaction.type
-          };
-          await transactionTypeService.InsertTransactionType(req, res, typeToAdd);
-          transactionTypesFromDb = await transactionTypeService.GetTransactionTypes();
-          existingTransactionTypes = await transactionTypesFromDb.rows.filter((x) => x.transactiontypename === transactionPlayer.transaction.type);
-        }
+          let existingPlayers = await playersFromDb.rows.filter((x) => x.yahooplayerid === Number(transactionPlayer.player_id) && x.gamecodetypeid === codeTypeId);
+          if (existingPlayers.length === 0) {
+            const playerToAdd = {
+              gamecodetypeid: codeTypeId,
+              yahooplayerid: transactionPlayer.player_id,
+              firstname: transactionPlayer.name.first,
+              lastname: transactionPlayer.name.last.length > 0 ? transactionPlayer.name.last : 'Defense',
+              positiontypeid: existingPositionType[0].positiontypeid
+            };
+            await playerSerivce.InsertPlayer(req, res, playerToAdd);
+            playersFromDb = await playerSerivce.GetPlayers();
+            existingPlayers = await playersFromDb.rows.filter((x) => x.yahooplayerid === Number(transactionPlayer.player_id) && x.gamecodetypeid === codeTypeId);
+          }
+          let existingTransactionTypes = await transactionTypesFromDb.rows.filter((x) => x.transactiontypename === transactionPlayer.transaction.type);
+          if (existingTransactionTypes.length === 0) {
+            const typeToAdd = {
+              transactiontypename: transactionPlayer.transaction.type
+            };
+            await transactionTypeService.InsertTransactionType(req, res, typeToAdd);
+            transactionTypesFromDb = await transactionTypeService.GetTransactionTypes();
+            existingTransactionTypes = await transactionTypesFromDb.rows.filter((x) => x.transactiontypename === transactionPlayer.transaction.type);
+          }
 
-        let fantasyteam;
-        let tradefromteam;
-        if (transactionPlayer.transaction.type === 'add') {
-          fantasyteam = teams.rows.filter((x) => x.team_key === transactionPlayer.transaction.destination_team_key);
-        } else if (transactionPlayer.transaction.type === 'drop') {
-          fantasyteam = teams.rows.filter((x) => x.team_key === transactionPlayer.transaction.source_team_key);
-        } else if (transactionPlayer.transaction.type === 'trade') {
-          fantasyteam = teams.rows.filter((x) => x.team_key === transactionPlayer.transaction.destination_team_key);
-          tradefromteam = teams.rows.filter((x) => x.team_key === transactionPlayer.transaction.source_team_key);
+          let fantasyteam;
+          let tradefromteam;
+          if (transactionPlayer.transaction.type === 'add') {
+            fantasyteam = teams.rows.filter((x) => x.team_key === transactionPlayer.transaction.destination_team_key);
+          } else if (transactionPlayer.transaction.type === 'drop') {
+            fantasyteam = teams.rows.filter((x) => x.team_key === transactionPlayer.transaction.source_team_key);
+          } else if (transactionPlayer.transaction.type === 'trade') {
+            fantasyteam = teams.rows.filter((x) => x.team_key === transactionPlayer.transaction.destination_team_key);
+            tradefromteam = teams.rows.filter((x) => x.team_key === transactionPlayer.transaction.source_team_key);
+          }
+          const transactionToImport = {
+            seasonid: seasonId,
+            fantasyteamid: fantasyteam[0].fantasyteamid,
+            playerid: existingPlayers[0].playerid,
+            transactiontypeid: existingTransactionTypes[0].transactiontypeid,
+            yahootransactionid: transaction.transaction_id,
+            tradefromteamid: tradefromteam ? tradefromteam[0].fantasyteamid : null,
+            transactiondate: new Date(Number(transaction.timestamp) * 1000)
+          };
+          transactionsToImport.push(transactionToImport);
         }
-        const transactionToImport = {
-          seasonid: seasonId,
-          fantasyteamid: fantasyteam[0].fantasyteamid,
-          playerid: existingPlayers[0].playerid,
-          transactiontypeid: existingTransactionTypes[0].transactiontypeid,
-          yahootransactionid: transaction.transaction_id,
-          tradefromteamid: tradefromteam ? tradefromteam[0].fantasyteamid : null,
-          transactiondate: new Date(Number(transaction.timestamp) * 1000)
-        };
-        transactionsToImport.push(transactionToImport);
       }
-
       if (transactionsToImport.length > 0) {
         await InsertTransactions(transactionsToImport);
         transactionsToImport = [];
